500a652dfc85e36392f41aeb115dd7713b7e92cf
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-05-11 19:45:44 +0200

    implement REST exception handling

--- org.javacream.training.books.service/src/main/java/org/javacream/training/books/service/web/BooksWebService.java
+++ org.javacream.training.books.service/src/main/java/org/javacream/training/books/service/web/BooksWebService.java
@@ -6,6 +6,7 @@ import org.javacream.training.books.service.api.Book;
 import org.javacream.training.books.service.api.BookException;
 import org.javacream.training.books.service.api.BooksService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,35 +15,52 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class BooksWebService {
 
-	@Autowired private BooksService booksService;
+	@Autowired
+	private BooksService booksService;
 
-	@PostMapping(path = "books" )
+	@PostMapping(path = "books")
 	public String newBook(@RequestParam("title") String title) throws BookException {
-		return booksService.newBook(title);
+		try {
+			return booksService.newBook(title);
+		} catch (Exception be) {
+			throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+		}
 	}
 
-	@GetMapping(path="books/{isbn}")
+	@GetMapping(path = "books/{isbn}", produces = "application/json")
 	public Book findBookByIsbn(@PathVariable("isbn") String isbn) throws BookException {
-		return booksService.findBookByIsbn(isbn);
+		try {
+			return booksService.findBookByIsbn(isbn);
+		} catch (Exception be) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
 	}
 
 	@PutMapping(path = "books", consumes = "application/json")
 	public Book updateBook(@RequestBody Book book) throws BookException {
-		return booksService.updateBook(book);
+		try {
+			return booksService.updateBook(book);
+		} catch (Exception be) {
+			throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+		}
 	}
 
-	@DeleteMapping(path="books/{isbn}")
+	@DeleteMapping(path = "books/{isbn}")
 	public void deleteBookByIsbn(@PathVariable("isbn") String isbn) throws BookException {
-		booksService.deleteBookByIsbn(isbn);
+		try {
+			booksService.deleteBookByIsbn(isbn);
+		} catch (Exception be) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT);
+		}
 	}
 
 	public Collection<Book> findAllBooks() {
 		return booksService.findAllBooks();
 	}
-	
-	
+
 }
