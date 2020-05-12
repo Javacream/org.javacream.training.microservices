a8c251902bc70df903a73bc6b3745f02071ce882
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-05-12 11:41:24 +0200

    implement JMX

--- org.javacream.training.store.service/src/main/java/org/javacream/training/store/service/StoreWebService.java
+++ org.javacream.training.store.service/src/main/java/org/javacream/training/store/service/StoreWebService.java
@@ -3,6 +3,9 @@ package org.javacream.training.store.service;
 import org.javacream.training.store.api.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +15,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @RestController
 @RequestMapping(path = "api")
+@ManagedResource(objectName = "javacream:type=service,name=store")
 public class StoreWebService {
+	
 	@Autowired private StoreService storeService;
 
 
 	@GetMapping(path = "store/{category}/{item}", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String getStock(@PathVariable("category") String category, @PathVariable("item") String item) {
+		invocationCounter++;
 		return Integer.toString(storeService.getStock(category, item));
 	}	
+	
+	private int invocationCounter;
+
+
+	@ManagedAttribute(description = "invocations")
+	public int getInvocationCounter() {
+		return invocationCounter;
+	}
+	
+	@ManagedOperation(description = "reset invocations")
+	public void resetCounter() {
+		invocationCounter = 0;
+	}
 	
 }
